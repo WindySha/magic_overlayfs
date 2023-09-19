9338abb0b55879cc52de8565906157461c2ee6fa
Wind <xiawanli2008@gmail.com>
2023-09-19 07:19:41 -0500

    Update mode.sh

--- magisk-module/mode.sh
+++ magisk-module/mode.sh
@@ -3,7 +3,7 @@
 # 1 - read-write default
 # 2 - read-only locked (cannot remount as read-write)
 # You can set to 2 after modify system partititons to avoid detection
-export OVERLAY_MODE=0
+export OVERLAY_MODE=1
 
 # Set to true to enable legacy mode that mount overlayfs on subdirectories instead of root partititons
 export OVERLAY_LEGACY_MOUNT=false

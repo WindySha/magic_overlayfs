ad1aef1056830c6242fa5dfa61806de61b957c6c
Wind <xiawanli2008@gmail.com>
2023-09-19 13:53:01 -0500

    Update partition.hpp

--- native/jni/partition.hpp
+++ native/jni/partition.hpp
@@ -7,6 +7,8 @@ std::vector<std::string> SYSTEM_PARTITIONS {
         "/odm", \
         "/vendor_dlkm", \
         "/odm_dlkm", \
+        "/my_custom", \
+        "/my_engineering", \
         "/my_heytap", \
         "/my_manifest", \
         "/my_preload", \
@@ -21,4 +23,6 @@ std::vector<std::string> SYSTEM_PARTITIONS {
         "/my_bigball", \
         "/prism", \
         "/optics", \
+        "/apex/com.android.art", \
+        "/apex/com.android.runtime", \
         "/oem" };
